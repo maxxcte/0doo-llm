bd397b645337d13c11521d7956fb1dc7c5ea4160
maxxcte <coinlist.hec1@gmail.com>
2022-07-18 11:36:24 +0700

    wip

--- llm/static/src/components/llm_message/llm_message.js
+++ llm/static/src/components/llm_message/llm_message.js
@@ -1,62 +1,53 @@
 /** @odoo-module **/
 
-import { Component, useRef } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 
 /**
- * Message component for displaying LLM chat messages
+ * Simplified message component for displaying LLM chat messages
  */
 export class LLMMessage extends Component {
   setup() {
-    this.contentRef = useRef("content");
-    this.prettyBodyRef = useRef("prettyBody");
     this.notification = useService("notification");
   }
 
   /**
-   * @returns {Object} The LLMMessage record from props
+   * @returns {Object} The message record from props
    */
   get message() {
     return this.props.record;
   }
 
   /**
-   * @returns {boolean} True if message is from user
+   * @returns {boolean} Whether message is from user
    */
   get isUserMessage() {
     return this.message.role === "user";
   }
 
   /**
-   * @returns {string} Name of message author
+   * @returns {string} Author name
    */
   get authorName() {
-    if (this.isUserMessage) {
-      return this.env.session.name;
-    }
-    return this.message.author || "Assistant";
+    return this.message.author || (this.isUserMessage ? this.env.session.name : "Assistant");
   }
 
   /**
-   * @returns {string} Formatted timestamp
+   * @returns {string} Formatted time
    */
   get formattedDate() {
     return this.message.formattedTime;
   }
 
   /**
-   * @returns {string} CSS class based on message status
+   * @returns {string} Status-based CSS class
    */
   get statusClass() {
     switch (this.message.status) {
-      case 'sending':
-        return 'text-muted';
-      case 'error':
-        return 'text-danger';
-      case 'sent':
-      default:
-        return '';
+      case 'sending': return 'text-muted';
+      case 'error': return 'text-danger';
+      default: return '';
     }
   }
 
@@ -64,26 +55,7 @@ export class LLMMessage extends Component {
    * @returns {boolean} Whether to show retry button
    */
   get showRetry() {
-    return this.message.status === 'error';
-  }
-
-  /**
-   * Handle mounting of content - renders markdown and code blocks
-   */
-  async onContentMounted() {
-    if (!this.contentRef.el || !this.prettyBodyRef.el || !this.message.content) {
-      return;
-    }
-
-        this.prettyBodyRef.el.textContent = this.message.content;
-
-      // Handle code blocks if any
-      const codeBlocks = this.prettyBodyRef.el.querySelectorAll('pre code');
-      if (window.hljs && codeBlocks.length) {
-        codeBlocks.forEach(block => {
-          window.hljs.highlightElement(block);
-        });
-      }
+    return this.message.status === 'error' && !this.isUserMessage;
   }
 
   /**
@@ -117,19 +89,9 @@ export class LLMMessage extends Component {
 LLMMessage.template = "llm.Message";
 
 LLMMessage.props = {
-  record: {
-    type: Object,
-    required: true,
-  },
-  className: {
-    type: String,
-    optional: true,
-  },
-  onRetry: {
-    type: Function,
-    optional: true,
-  },
+  record: { type: Object, required: true },
+  className: { type: String, optional: true },
+  onRetry: { type: Function, optional: true },
 };
 
-// Register the component
 registry.category("components").add("LLMMessage", LLMMessage);
