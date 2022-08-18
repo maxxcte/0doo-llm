32908e6a5d29d111592e3520b5e6cc365b800071
maxxcte <coinlist.hec1@gmail.com>
2022-08-18 16:39:03 +0700

    working wip

--- llm_thread/__manifest__.py
+++ llm_thread/__manifest__.py
@@ -1,57 +1,42 @@
 {
-    "name": "LLM Chat",
-    "summary": """
-        Chat with various LLM providers like Ollama, OpenAI, Replicate and Anthropic""",
+    "name": "LLM Thread",
+    "summary": "Message thread support for LLM conversations",
     "description": """
-        Provides integration with LLM (Large Language Model) providers for:
-        - Chat completions
-        - Text embeddings
-        - Model management
-
-        Supported providers:
-        - OpenAI
-        - Ollama
-        - Replicate
-        - Anthropic
+        Extends the LLM integration module with conversation threading capabilities:
+        - Persistent chat history
+        - Real-time streaming responses
+        - Message management
+        - Thread organization
+        - Chat export
     """,
-    "author": "Apexive Solutions LLC",
-    "website": "https://github.com/apexive/odoo-llm",
     "category": "Technical",
-    "version": "16.0.1.0.3",
+    "version": "16.0.1.0.0",
     "depends": ["llm", "mail"],
+    "external_dependencies": {},
     "data": [
-        "security/llm_security.xml",
+        "security/llm_thread_security.xml",
         "security/ir.model.access.csv",
-        "views/llm_views.xml",
         "views/llm_thread_views.xml",
     ],
-    "license": "LGPL-3",
     "assets": {
         "web.assets_backend": [
-            # Base Models
-            "llm/static/src/components/models.js",
             # Components
-            "llm/static/src/components/llm_message/llm_message.js",
-            "llm/static/src/components/llm_message/llm_message.scss",
-            "llm/static/src/components/llm_message/llm_message.xml",
-            "llm/static/src/components/llm_message_list/llm_message_list.js",
-            "llm/static/src/components/llm_message_list/llm_message_list.scss",
-            "llm/static/src/components/llm_message_list/llm_message_list.xml",
-            "llm/static/src/components/llm_composer/llm_composer.js",
-            "llm/static/src/components/llm_composer/llm_composer.scss",
-            "llm/static/src/components/llm_composer/llm_composer.xml",
-            "llm/static/src/components/llm_thread_view/llm_thread_view.js",
-            "llm/static/src/components/llm_thread_view/llm_thread_view.scss",
-            "llm/static/src/components/llm_thread_view/llm_thread_view.xml",
-            "llm/static/src/components/llm_chat_dialog/llm_chat_dialog.js",
-            "llm/static/src/components/llm_chat_dialog/llm_chat_dialog.xml",
+            "llm_thread/static/src/components/llm_message/llm_message.js",
+            "llm_thread/static/src/components/llm_message/llm_message.scss",
+            "llm_thread/static/src/components/llm_message/llm_message.xml",
+            "llm_thread/static/src/components/llm_message_list/llm_message_list.js",
+            "llm_thread/static/src/components/llm_message_list/llm_message_list.scss",
+            "llm_thread/static/src/components/llm_message_list/llm_message_list.xml",
+            "llm_thread/static/src/components/llm_composer/llm_composer.js",
+            "llm_thread/static/src/components/llm_composer/llm_composer.scss",
+            "llm_thread/static/src/components/llm_composer/llm_composer.xml",
+            "llm_thread/static/src/components/llm_thread_view/llm_thread_view.js",
+            "llm_thread/static/src/components/llm_thread_view/llm_thread_view.scss",
+            "llm_thread/static/src/components/llm_thread_view/llm_thread_view.xml",
+            "llm_thread/static/src/components/llm_chat_dialog/llm_chat_dialog.js",
+            "llm_thread/static/src/components/llm_chat_dialog/llm_chat_dialog.xml",
         ],
     },
-    "external_dependencies": {
-        "python": ["openai", "replicate", "ollama", "anthropic"],
-    },
+    "license": "LGPL-3",
     "installable": True,
-    "images": [
-        "static/description/banner.jpeg",
-    ],
 }
--- llm_thread/models/llm_thread.py
+++ llm_thread/models/llm_thread.py
@@ -1,28 +1,33 @@
+from odoo import api, fields, models
 import logging
 
-from odoo import api, fields, models
-
 _logger = logging.getLogger(__name__)
-
 
 class LLMThread(models.Model):
     _name = "llm.thread"
     _description = "LLM Chat Thread"
+    _inherit = ["mail.thread"]
     _order = "write_date DESC"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(
+        string="Title",
+        required=True,
+        tracking=True,
+    )
     user_id = fields.Many2one(
         "res.users",
         string="User",
         default=lambda self: self.env.user,
         required=True,
         ondelete="restrict",
+        tracking=True,
     )
     provider_id = fields.Many2one(
         "llm.provider",
         string="Provider",
         required=True,
         ondelete="restrict",
+        tracking=True,
     )
     model_id = fields.Many2one(
         "llm.model",
@@ -30,25 +35,23 @@ class LLMThread(models.Model):
         required=True,
         domain="[('provider_id', '=', provider_id), ('model_use', 'in', ['chat', 'multimodal'])]",
         ondelete="restrict",
+        tracking=True,
     )
-    message_ids = fields.One2many("mail.message", "thread_id", string="Messages")
-    active = fields.Boolean(default=True)
+    active = fields.Boolean(default=True, tracking=True)
+    message_ids = fields.One2many(
+        comodel_name="mail.message",
+        inverse_name="res_id",
+        string="Messages",
+        domain=lambda self: [('model', '=', self._name)],
+    )
 
     @api.model_create_multi
     def create(self, vals_list):
-        # Set default title if not provided
+        """Set default title if not provided"""
         for vals in vals_list:
             if not vals.get("name"):
                 vals["name"] = f"Chat {fields.Datetime.now()}"
         return super().create(vals_list)
-
-    def get_chat_messages(self, limit=None):
-        """Get messages in provider-compatible format"""
-        domain = [("thread_id", "=", self.id)]
-        messages = self.env["llm.message"].search(
-            domain, order="create_date ASC", limit=limit
-        )
-        return [msg.to_provider_message() for msg in messages]
 
     def action_open_chat(self):
         """Open chat in dialog"""
@@ -81,94 +84,82 @@ class LLMThread(models.Model):
         }
 
     def post_message(self, content, role="user"):
-        """Simply post a message to the thread"""
+        """Post a message to the thread"""
         _logger.debug("Posting message - role: %s, content: %s", role, content)
 
-        message = self.env["llm.message"].create(
-            {
-                "thread_id": self.id,
-                "content": content,
-                "role": role,
-            }
-        )
+        message = self.env["mail.message"].create({
+            "model": self._name,
+            "res_id": self.id,
+            "body": content,
+            "message_type": "comment",
+            "llm_role": role,
+        })
         return message
+
+    def get_chat_messages(self, limit=None):
+        """Get messages in provider-compatible format"""
+        domain = [
+            ("model", "=", self._name),
+            ("res_id", "=", self.id),
+            ("message_type", "=", "comment"),
+        ]
+        messages = self.env["mail.message"].search(
+            domain, order="create_date ASC", limit=limit
+        )
+        return [msg.to_provider_message() for msg in messages]
 
     def get_assistant_response(self, stream=True):
         """Get streaming response from assistant based on conversation history"""
         try:
-            # Get conversation history
             messages = self.get_chat_messages()
             _logger.debug("Getting assistant response for messages: %s", messages)
 
-            # Get AI response
             content = ""
             for response in self.model_id.chat(messages, stream=stream):
                 if response.get("content"):
                     content += response.get("content", "")
                     yield response
 
-            # Save final AI response if we have content
             if content:
                 _logger.debug("Saving assistant response: %s", content)
-                self.env["llm.message"].create(
-                    {
-                        "thread_id": self.id,
-                        "content": content,
-                        "role": "assistant",
-                    }
-                )
+                self.post_message(content=content, role="assistant")
 
         except Exception as e:
             _logger.error("Error getting AI response: %s", str(e))
             yield {"error": str(e)}
 
-
 class MailMessage(models.Model):
     _inherit = "mail.message"
-    _order = "create_date ASC"
-    # TODO extend subtypes to include "llm"
-    llm_role = fields.Selection(
-        [
-            ("system", "System"),
-            ("user", "User"),
-            ("assistant", "Assistant"),
-            ("tool", "Tool"),
-        ],
-        default="user",
-    )
+
+    llm_role = fields.Selection([
+        ("system", "System"),
+        ("user", "User"),
+        ("assistant", "Assistant"),
+        ("tool", "Tool"),
+    ], default="user")
 
     def to_provider_message(self):
         """Convert to provider-compatible message format"""
         return {
             "role": self.llm_role,
-            "content": self.content,
+            "content": self.body,
         }
 
     def to_frontend_data(self):
         """Convert to frontend-friendly format"""
         return {
             "id": self.id,
-            "role": self.role,
-            "content": self.content,
+            "role": self.llm_role,
+            "content": self.body,
             "timestamp": fields.Datetime.to_string(self.create_date),
             "author": self.get_author_name(),
         }
 
     def get_author_name(self):
         """Get author name based on role"""
-        if self.role == "user":
-            return self.thread_id.user_id.name
-        elif self.role == "assistant":
-            return self.thread_id.model_id.name
-        return self.role.title()
-
-    @api.model
-    def from_provider_message(self, thread_id, message):
-        """Create from provider message format"""
-        return self.create(
-            {
-                "thread_id": thread_id,
-                "role": message["role"],
-                "content": message["content"],
-            }
-        )
+        thread = self.env["llm.thread"].browse(self.res_id)
+        if self.llm_role == "user":
+            return thread.user_id.name
+        elif self.llm_role == "assistant":
+            return thread.model_id.name
+        return self.llm_role.title()
