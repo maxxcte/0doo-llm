6423e96dd2378e49b65a2c571b68fa169e7ec2c6
maxxcte <coinlist.hec1@gmail.com>
2022-09-07 15:59:29 +0700

    clean up everything just keep essential

--- llm_thread/__manifest__.py
+++ llm_thread/__manifest__.py
@@ -16,29 +16,9 @@
     "data": [
         "security/llm_thread_security.xml",
         "security/ir.model.access.csv",
-        "views/llm_model_views.xml",
         "views/llm_thread_views.xml",
-        "views/llm_mail_channel_views.xml",
+        
     ],
-    "assets": {
-        "web.assets_backend": [
-            # Components
-            "llm_thread/static/src/components/llm_message/llm_message.js",
-            "llm_thread/static/src/components/llm_message/llm_message.scss",
-            "llm_thread/static/src/components/llm_message/llm_message.xml",
-            "llm_thread/static/src/components/llm_message_list/llm_message_list.js",
-            "llm_thread/static/src/components/llm_message_list/llm_message_list.scss",
-            "llm_thread/static/src/components/llm_message_list/llm_message_list.xml",
-            "llm_thread/static/src/components/llm_composer/llm_composer.js",
-            "llm_thread/static/src/components/llm_composer/llm_composer.scss",
-            "llm_thread/static/src/components/llm_composer/llm_composer.xml",
-            "llm_thread/static/src/components/llm_thread_view/llm_thread_view.js",
-            "llm_thread/static/src/components/llm_thread_view/llm_thread_view.scss",
-            "llm_thread/static/src/components/llm_thread_view/llm_thread_view.xml",
-            "llm_thread/static/src/components/llm_chat_dialog/llm_chat_dialog.js",
-            "llm_thread/static/src/components/llm_chat_dialog/llm_chat_dialog.xml",
-        ],
-    },
     "license": "LGPL-3",
     "installable": True,
 }
--- llm_thread/models/__init__.py
+++ llm_thread/models/__init__.py
@@ -1,4 +1,1 @@
 from . import llm_thread
-from . import llm_model
-from . import res_users
-from . import res_users_settings
