853e537a93668c5ef107dfc6628fd7cf29d64e1d
maxxcte <coinlist.hec1@gmail.com>
2022-12-15 10:40:29 +0700

    debug with llm

--- llm_thread/static/src/components/llm_chat_message_list/llm_chat_message_list.js
+++ llm_thread/static/src/components/llm_chat_message_list/llm_chat_message_list.js
@@ -4,13 +4,41 @@ import { MessageList } from '@mail/components/message_list/message_list';
 import { Transition } from '@web/core/transition';
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
 import { markup } from '@odoo/owl';
+import { useEffect } from '@odoo/owl';
+
 export class LLMChatMessageList extends MessageList {
+    setup() {
+        super.setup();
+        useEffect(() => {
+            if (this.composerView.isStreaming && this.htmlStreamingContent) {
+                console.log("Triggered useEffect - isStreaming:", this.composerView.isStreaming, "htmlStreamingContent:", this.htmlStreamingContent);
+                this._scrollToEnd();
+            }
+        }, () => [this.htmlStreamingContent]);
+    }
+
     get htmlStreamingContent() {
-        return markup(this.composerView.htmlStreamingContent);
+        return this.composerView.htmlStreamingContent ? markup(this.composerView.htmlStreamingContent) : '';
     }
 
     get composerView() {
         return this.props.composerView;
+    }
+
+    _scrollToEnd() {
+        const { messageListView, order } = this._lastRenderedValues();
+        const scrollable = messageListView.getScrollableElement();
+        console.log("scrollToEnd called - scrollable:", scrollable, "order:", order);
+        if (scrollable) {
+            const scrollHeight = scrollable.scrollHeight;
+            const clientHeight = scrollable.clientHeight;
+            const scrollTop = order === 'asc' ? scrollHeight - clientHeight : 0;
+            console.log("Scroll details - scrollHeight:", scrollHeight, "clientHeight:", clientHeight, "scrollTop:", scrollTop);
+            scrollable.scrollTop = scrollTop;
+            console.log("After scroll - current scrollTop:", scrollable.scrollTop);
+        } else {
+            console.log("No scrollable element found!");
+        }
     }
 }
 
