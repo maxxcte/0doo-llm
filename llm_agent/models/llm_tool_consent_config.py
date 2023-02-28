44b5a400a3b48e157be5ec57539303aed6c7621d
maxxcte <coinlist.hec1@gmail.com>
2023-02-28 13:09:33 +0700

    remove redundant codes

--- llm_agent/models/llm_tool_consent_config.py
+++ llm_agent/models/llm_tool_consent_config.py
@@ -1,15 +1,13 @@
-import logging
-
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
-_logger = logging.getLogger(__name__)
 class LLMToolConsentConfig(models.Model):
     """Configuration for tool consent messages"""
     _name = "llm.tool.consent.config"
     _description = "LLM Tool Consent Configuration"
-
+    _rec_name = 'name'
     name = fields.Char(required=True, default="Default Configuration")
-    active = fields.Boolean(default=True)
+    active = fields.Boolean(default=False)
     
     # Message to add to tool description
     tool_description_message = fields.Text(
@@ -28,10 +26,32 @@ For these tools, you MUST:
 4. If the user denies consent or doesn't respond clearly, do not use the tool"""
     )
     
+    @api.constrains('active')
+    def _check_active_unique(self):
+        """Ensure only one configuration is active at a time"""
+        for record in self.filtered('active'):
+            # Count active records excluding the current one
+            active_count = self.search_count([
+                ('active', '=', True),
+                ('id', '!=', record.id)
+            ])
+            if active_count > 0:
+                raise ValidationError(_("Only one configuration can be active at a time."))
+    
     @api.model
     def get_active_config(self):
         """Get the active configuration or create default if none exists"""
         config = self.search([('active', '=', True)], limit=1)
         if not config:
-            config = self.create({})
+            # No active config, try to find any config
+            config = self.search([], limit=1)
+            if config:
+                # Activate the first config found
+                config.write({'active': True})
+            else:
+                # Create a new default config and activate it
+                config = self.create({
+                    'name': 'Default Configuration',
+                    'active': True
+                })
         return config
