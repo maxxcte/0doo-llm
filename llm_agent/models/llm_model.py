8b00ebe772fa4b998d42b8837a4d308067360111
maxxcte <coinlist.hec1@gmail.com>
2023-03-15 10:24:03 +0700

    remove wrong function override

--- llm_agent/models/llm_model.py
+++ llm_agent/models/llm_model.py
@@ -9,7 +9,3 @@ class LLMModel(models.Model):
         return self.provider_id.chat(
             messages, model=self, stream=stream, tools=tools, tool_choice=tool_choice
         )
-
-    def embedding(self, texts):
-        """Generate embeddings using this model"""
-        return self.provider_id.embedding(texts, model=self)
