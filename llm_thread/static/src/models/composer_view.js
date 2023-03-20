79c1e420c8d7669e034a071fc5054594fe9f9997
maxxcte <coinlist.hec1@gmail.com>
2023-03-20 11:03:35 +0700

    update compoer_viser

--- llm_thread/static/src/models/composer_view.js
+++ llm_thread/static/src/models/composer_view.js
@@ -20,56 +20,131 @@ registerPatch({
         return markdownToHtml(this.streamingContent);
       },
     }),
+    // Tool handling related fields
+    isToolActive: attr({
+      default: false,
+    }),
+    currentToolCallId: attr({
+      default: false,
+    }),
+    currentToolName: attr({
+      default: "",
+    }),
+    toolArguments: attr({
+      default: "",
+    }),
+    toolResult: attr({
+      default: "",
+    }),
+    // Flag to track if the current streaming content is from a tool
+    isToolContent: attr({
+      default: false,
+    }),
+    // Flag to prevent multiple interpretation requests
+    isInterpretationRequested: attr({
+      default: false,
+    }),
+    pendingToolMessages: many("LLMToolMessage", {
+      inverse: "composerView",
+    }),
   },
   recordMethods: {
     /**
-     * Post AI message to the thread
+     * Post a message to the thread
+     * @param {String} content - HTML content to post
+     * @param {String} toolCallId - Optional tool call ID for tool messages
      * @private
-     * @param {String} body - The message body
      */
-    async _postAIMessage(body) {
-      const composer = this.composer;
-      const params = {
-        thread_id: composer.thread.id,
-        body,
+    async _postAIMessage(content, toolCallId = false) {
+      const threadId = this.composer.thread.id;
+      const data = {
+        body: content,
+        thread_id: threadId,
       };
-      const messaging = this.messaging;
-      const messageData = await messaging.rpc(
-        { route: `/llm/thread/post_ai_response`, params },
-        { shadow: true }
-      );
-      if (!messaging.exists()) {
-        return;
+
+      // If this is a tool message, add the tool_call_id and subtype
+      if (toolCallId) {
+        data.tool_call_id = toolCallId;
+        data.subtype_xmlid = "llm_tool.mt_tool_message";
+
+        // Find the tool message to get the function name
+        const toolMessage = this.pendingToolMessages.find(
+          (msg) => msg.toolCallId === toolCallId
+        );
+        if (toolMessage) {
+          data.tool_name = toolMessage.functionName;
+        }
       }
-      const message = messaging.models.Message.insert(
-        messaging.models.Message.convertData(messageData)
-      );
-      if (messaging.hasLinkPreviewFeature && !message.isBodyEmpty) {
-        messaging.rpc(
-          {
-            route: `/mail/link_preview`,
-            params: {
-              message_id: message.id,
-            },
-          },
+
+      const messaging = this.messaging;
+      try {
+        const messageData = await messaging.rpc(
+          { route: `/llm/thread/post_ai_response`, params: data },
           { shadow: true }
         );
-      }
-      for (const threadView of message.originThread.threadViews) {
-        // Reset auto scroll to be able to see the newly posted message.
-        threadView.update({ hasAutoScrollOnMessageReceived: true });
-        threadView.addComponentHint("message-posted", { message });
+
+        if (!messaging.exists()) {
+          return;
+        }
+
+        const message = messaging.models.Message.insert(
+          messaging.models.Message.convertData(messageData)
+        );
+
+        if (messaging.hasLinkPreviewFeature && !message.isBodyEmpty) {
+          messaging.rpc(
+            {
+              route: `/mail/link_preview`,
+              params: {
+                message_id: message.id,
+              },
+            },
+            { shadow: true }
+          );
+        }
+
+        for (const threadView of message.originThread.threadViews) {
+          // Reset auto scroll to be able to see the newly posted message.
+          threadView.update({ hasAutoScrollOnMessageReceived: true });
+          threadView.addComponentHint("message-posted", { message });
+        }
+
+        // Clear the pending tool message after it's posted
+        if (toolCallId) {
+          const messageToRemove = this.pendingToolMessages.find(
+            (msg) => msg.toolCallId === toolCallId
+          );
+          if (messageToRemove) {
+            messageToRemove.delete();
+          }
+        }
+
+        return message;
+      } catch (error) {
+        console.error("Error posting message:", error);
       }
     },
 
     /**
      * Stop streaming response for this thread
      */
-    async _stopStreaming() {
-      if (!this.isStreaming) {
-        return;
+    _stopStreaming() {
+      // Delete all pending tool messages
+      for (const toolMessage of this.pendingToolMessages) {
+        toolMessage.delete();
       }
-      this.update({ isStreaming: false, streamingContent: "" });
+
+      this.update({
+        isStreaming: false,
+        streamingContent: "",
+        isToolActive: false,
+        currentToolCallId: false,
+        currentToolName: "",
+        toolArguments: "",
+        toolResult: "",
+        isToolContent: false,
+        isInterpretationRequested: false,
+      });
     },
     /**
      * Start streaming response for this thread
@@ -81,7 +156,17 @@ registerPatch({
       }
       const composer = this.composer;
 
-      this.update({ isStreaming: true, streamingContent: defaultContent });
+      // Delete any existing pending tool messages
+      for (const toolMessage of this.pendingToolMessages) {
+        toolMessage.delete();
+      }
+
+      this.update({
+        isStreaming: true,
+        streamingContent: defaultContent,
+        isToolContent: false,
+      });
+
       const eventSource = new EventSource(
         `/llm/thread/stream_response?thread_id=${composer.thread.id}`
       );
@@ -99,6 +184,39 @@ registerPatch({
               streamingContent: this.streamingContent + (data.content || ""),
             });
             break;
+          case "tool_start":
+            // Handle tool start event
+            this.update({
+              isToolActive: true,
+              currentToolCallId: data.tool_call_id,
+              currentToolName: data.function_name,
+              toolArguments: data.arguments,
+              // Clear streaming content for tool
+              streamingContent: "",
+              // Mark that we're now dealing with tool content
+              isToolContent: true,
+            });
+            break;
+          case "tool_end":
+            // Handle tool end event
+            this.update({
+              toolResult: data.content,
+              isToolActive: false,
+            });
+
+            console.log("Tool ended");
+
+            // Create a new LLMToolMessage record with tool_call_id as the identifier
+            this.messaging.models.LLMToolMessage.insert({
+              id: data.tool_call_id,
+              content: markdownToHtml(data.formatted_content),
+              toolCallId: data.tool_call_id,
+              functionName: this.currentToolName,
+              arguments: this.toolArguments,
+              result: data.content,
+              composerView: this,
+            });
+            break;
           case "error":
             console.error("Streaming error:", data.error);
             eventSource.close();
@@ -109,12 +227,8 @@ registerPatch({
             });
             break;
           case "end":
-            {
-              const htmlStreamingContent = this.htmlStreamingContent;
-              eventSource.close();
-              await this._postAIMessage(htmlStreamingContent);
-              this._stopStreaming();
-            }
+            eventSource.close();
+            this._handleStreamingEnd();
             break;
         }
       };
@@ -125,6 +239,79 @@ registerPatch({
         this._stopStreaming();
       };
     },
+
+    /**
+     * Handle the end of a streaming session
+     * Post any content and tool messages, then start interpretation if needed
+     * @private
+     */
+    async _handleStreamingEnd() {
+      // Post any regular content if we have some and it's not tool-related content
+      if (this.streamingContent && !this.isToolContent) {
+        const htmlStreamingContent = this.htmlStreamingContent;
+        await this._postAIMessage(htmlStreamingContent);
+      }
+
+      // Post all pending tool messages
+      if (this.pendingToolMessages.length > 0) {
+        console.log(
+          `Posting ${this.pendingToolMessages.length} pending tool messages`
+        );
+
+        // Post all tool messages in sequence
+        for (const toolMessage of this.pendingToolMessages) {
+          await this._postAIMessage(
+            toolMessage.content,
+            toolMessage.toolCallId
+          );
+        }
+
+        // Only start interpretation after all tool messages are posted
+        // and if this is the initial streaming (not already interpreting)
+        if (this.isToolContent && !this.isInterpretationRequested) {
+          console.log("Starting interpretation streaming");
+
+          // First, clean up the current streaming session
+          // but preserve the isToolContent flag
+          const wasToolContent = this.isToolContent;
+          this._stopStreaming();
+
+          // Then set up for interpretation
+          this.update({
+            isInterpretationRequested: true,
+            isToolContent: wasToolContent,
+          });
+
+          // Small delay to ensure messages are fully processed
+          setTimeout(() => this.startInterpretationStreaming(), 500);
+          // Exit early, we'll handle the interpretation streaming separately
+          return;
+        }
+      }
+
+      // If we get here, either:
+      // 1. This is the end of a regular streaming session with no tool calls
+      // 2. This is the end of an interpretation streaming session
+      console.log(
+        "Ending streaming session, isInterpretation:",
+        this.isInterpretationRequested
+      );
+      this._stopStreaming();
+    },
+
+    /**
+     * Start streaming for interpretation after tool calls
+     */
+    startInterpretationStreaming() {
+      // Set flag to prevent multiple interpretation requests
+      this.update({ isInterpretationRequested: true });
+
+      // Start a new streaming session for interpretation
+      this.startStreaming();
+
+      console.log("Started interpretation streaming");
+    },
+
     async postUserMessageForAi() {
       await this.postMessage();
       this.update({
