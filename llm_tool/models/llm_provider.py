5172dfa06edb1b4e11331ce591b567f0d9bf9667
maxxcte <coinlist.hec1@gmail.com>
2023-04-14 11:35:16 +0700

    remove duplicate method override

--- llm_tool/models/llm_provider.py
+++ llm_tool/models/llm_provider.py
@@ -11,14 +11,3 @@ class LLMProvider(models.Model):
     def format_tools(self, tools):
         """Format tools for the specific provider"""
         return self._dispatch("format_tools", tools)
-
-    def format_messages(self, messages):
-        """Format messages for the specific provider
-
-        Args:
-            messages: mail.message recordset to format
-
-        Returns:
-            List of formatted messages in provider-specific format
-        """
-        return self._dispatch("format_messages", messages)
