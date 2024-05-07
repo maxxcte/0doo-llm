f7a8c581f29a20842d8441ab5c7c8cf26089d989
maxxcte <coinlist.hec1@gmail.com>
2024-05-07 09:54:37 +0700

    move posting llm message via Composer, rather than ComposerView

--- llm_thread/static/src/models/composer.js
+++ llm_thread/static/src/models/composer.js
@@ -15,5 +15,56 @@ registerPatch({
       },
       default: true, // Assume disabled initially
     }),
+    eventSource: attr({
+      default: null,
+    }),
   },
+  recordMethods: {
+    stopLLMThreadLoop() {
+      // this should close event source
+      this._closeEventSource();
+    },
+    async postUserMessageForLLM() {
+      const thread = this.thread;
+      
+      const messageBody = this.textInputContent.trim();
+      if (!messageBody || !thread) {
+          return; // Or show warning
+      }
+  
+      this.update({ textInputContent: '' });
+  
+      try {
+          this.eventSource = new EventSource(
+            `/llm/thread/generate?thread_id=${thread.id}&message=${messageBody}`,
+          );
+          this.eventSource.onmessage = async (event) =>{
+            const data = JSON.parse(event.data);
+            switch (data.type) {
+              case 'end':
+                this._closeEventSource();
+                break;
+            }
+          }
+          this.eventSource.onerror = (error) => {
+            console.error("EventSource failed:", error);
+            this._closeEventSource();
+          };
+      } catch (error) {
+          console.error("Error sending LLM message:", error);
+          this.messaging.notify({ message: this.env._t("Failed to send message."), type: 'danger' });
+      } finally {
+        for (const composerView of this.composerViews) {
+          composerView.update({ doFocus: true });
+        }
+      }
+    },
+  },
+
+  _closeEventSource(){
+    if(this.eventSource){
+      this.eventSource.close();
+      this.update({ eventSource: null });
+    }
+  }
 });
--- llm_thread/static/src/models/composer_view.js
+++ llm_thread/static/src/models/composer_view.js
@@ -5,67 +5,6 @@ import { registerPatch } from "@mail/model/model_core";
 registerPatch({
   name: "ComposerView",
   recordMethods: {
-    async _updateLLMThread(params){
-      const thread = this.composer.thread;
-      
-      try {
-        if (!thread || !thread.id) {
-          throw new Error("Thread or Thread ID is missing.");
-        }
-        const result = await this.messaging.rpc({
-            route: `/llm/thread/${thread.id}/update`, // Use the new route
-            params: params,
-        });
-        if (result.status === 'error') {
-            this.messaging.notify({ message: result.error, type: 'danger' });
-        } else if (result.status === 'success') {
-           console.log("updated LLM Thread successfully");
-        }
-      } catch (error) {
-          console.error("Error updating LLMThread:", error);
-          this.messaging.notify({ message: this.env._t("Failed to update LLM Thread."), type: 'danger' });
-      } finally {
-          this.update({ doFocus: true });
-      }
-    },
-    async stopLLMThreadLoop() {
-      // this should close event source
-    },
-
-    async postUserMessageForLLM() {
-      const thread = this.composer.thread;
-      
-      const messageBody = this.composer.textInputContent.trim();
-      if (!messageBody || !thread) {
-          return; // Or show warning
-      }
-  
-      this.composer.update({ textInputContent: '' });
-  
-      try {
-          const eventSource = new EventSource(
-            `/llm/thread/run?thread_id=${thread.id}&message=${messageBody}`,
-          );
-          eventSource.onmessage = async (event) =>{
-            const data = JSON.parse(event.data);
-            switch (data.type) {
-              case 'end':
-                eventSource.close();
-                break;
-            }
-          }
-          eventSource.onerror = (error) => {
-            console.error("EventSource failed:", error);
-            eventSource.close();
-          };
-      } catch (error) {
-          console.error("Error sending LLM message:", error);
-          this.messaging.notify({ message: this.env._t("Failed to send message."), type: 'danger' });
-      } finally {
-           this.update({ doFocus: true });
-      }
-    },
-
     onKeydownTextareaForLLM(ev) {
       if (!this.exists()) {
         return;
@@ -125,7 +64,7 @@ registerPatch({
     _handleSendShortcuts(ev) {
       for (const shortcut of this.sendShortcuts) {
         if (this._matchesShortcut(ev, shortcut)) {
-          this.postUserMessageForLLM();
+          this.composer.postUserMessageForLLM();
           ev.preventDefault();
           return true;
         }
