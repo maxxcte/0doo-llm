46158b7394587925b49fb46db7bebd02ceea18fa
maxxcte <coinlist.hec1@gmail.com>
2024-08-20 10:07:38 +0700

    fix transaction error for tool call aborting whole transaction

--- llm_thread/models/mail_message.py
+++ llm_thread/models/mail_message.py
@@ -130,11 +130,14 @@ class MailMessage(models.Model):
 
         # 2) execute + update
         try:
-            result = thread._execute_tool(name, args)
-            if not result:
-                raise UserError(f"No result returned from tool '{name}'")
-            body   = f"Result for {name}"
-            write_vals = {"tool_call_result": json.dumps(result), "body": body}
+            # to isolate tool call, otherwise transaction error can cause the transaction to fail
+            # and the transaction will be rolled back(aborted state)
+            with self.env.cr.savepoint():
+                result = thread._execute_tool(name, args)
+                if not result:
+                    raise UserError(f"No result returned from tool '{name}'")
+                body   = f"Result for {name}"
+                write_vals = {"tool_call_result": json.dumps(result), "body": body}
         except Exception as e:
             write_vals = {
                 "tool_call_result": json.dumps({"error": str(e)}),
