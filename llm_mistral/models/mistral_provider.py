31dd906ef8e6d446eac491ac13a39ab26683fcd0
maxxcte <coinlist.hec1@gmail.com>
2024-10-04 12:32:40 +0700

    detect correct model capabilities for mistral

--- llm_mistral/models/mistral_provider.py
+++ llm_mistral/models/mistral_provider.py
@@ -14,3 +14,34 @@ class LLMProvider(models.Model):
 
     def _dispatch_on_message(self, message_record, method, *args, **kwargs):
         return super()._dispatch_on_message(message_record, method, *args, service_override=("openai" if self.service == "mistral" else None), **kwargs)
+        
+    def openai_models(self):
+        if self.service == "mistral":
+            models = self.client.models.list()
+
+            for model in models.data:
+                model_json_dump = model.model_dump()
+                capabilities = []
+                model_caps = model_json_dump["capabilities"]
+                if model_caps["vision"]:
+                    capabilities.append("multimodal")
+                elif model_caps["completion_chat"]:
+                    capabilities.append("chat")
+                elif "ocr" in model.id:
+                    capabilities.append("ocr")
+                elif "embed" in model.id:
+                    capabilities.append("embedding")
+                else:
+                    capabilities.append("chat")
+
+                model_json_dump.pop("capabilities", None)
+                yield {
+                    "name": model.id,
+                    "details": {
+                        "id": model.id,
+                        "capabilities": capabilities,
+                        **model_json_dump,
+                    },
+                }
+        else:
+            return super().openai_models()
